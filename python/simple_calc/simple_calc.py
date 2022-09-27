3678348a7dc535b2cfbfbef78e05fd8445979efa
root <root@beaglebone.localdomain>
2022-09-27 05:44:49 +0000

    added blink_led folder and blink_USR3.py file

--- python/simple_calc/simple_calc.py
+++ python/simple_calc/simple_calc.py
@@ -4,7 +4,7 @@
 Simple Calculator
 --------------------------------------------------------------------------
 License:   
-Copyright 2022 - <NAME>
+Copyright 2022 - Eunice Tan
 
 Redistribution and use in source and binary forms, with or without 
 modification, are permitted provided that the following conditions are met:
