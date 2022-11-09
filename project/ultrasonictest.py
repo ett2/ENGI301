398301088fbe2f94aa60bdd50b55d3294470d750
Demo User <debian@beaglebone.localdomain>
2022-11-09 21:43:38 +0000

    updated ultrasonic test

--- project/ultrasonictest.py
+++ project/ultrasonictest.py
@@ -1,37 +1,76 @@
 #!/usr/bin/env python3
 
-# This is an example of reading HC-SR04 Ultrasonic Range Finder
-# This version measures from the fall of the Trigger pulse 
-#   to the end of the Echo pulse
-# Check the timing to be sure it calibrated.
+# When the car is in an autonomous state, run this code
+# if it ever exits an autonomous state, exit this code
+# if the car gets too close to something else, also exit this code
 
 import Adafruit_BBIO.GPIO as GPIO
 import time
 
-trigger = 'P9_15'    # Pin to trigger the ultrasonic pulse
-echo    = 'P9_16'    # Pin to measure to pulse width related to the distance
+trigger0 = 'P9_15'    # Pin to trigger the ultrasonic pulse
+echo0    = 'P9_16'    # Pin to measure to pulse width related to the distance
+trigger1 = 'P9_17'
+echo1    = 'P9_18'
+
 ms = 1000            # Trigger period in ms
 
-GPIO.setup(trigger, GPIO.OUT)
-GPIO.setup(echo, GPIO.IN)
-print("sss")
-# Called when falling edte of pulse is detected
-def pingEnd(channel):
-    global startTime
-    # print('Edge detected on channel %s'%channel)
-    totalTime = time.perf_counter() - startTime
-    print('totalTime = {0:0.2f}'.format(1000*totalTime))
-    # startTime = time.perf_counter()
+# Triggers the breaking state
+brake = False
 
-GPIO.add_event_detect(echo, GPIO.FALLING, callback=pingEnd) 
-
-# Pulse trigger high then low and start timing.
-while True:             # Do something while waiting for event
-    #print('ping')
-    GPIO.output(trigger, 1)
-    time.sleep(0.005)
-    GPIO.output(trigger, 0)
-    startTime = time.perf_counter()
-    time.sleep(ms/1000)         # Wait and let callback to the work
-    
 GPIO.cleanup()
+time.sleep(2)
+
+
+def distance_measurement(TRIG,ECHO):
+    GPIO.output(TRIG, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG, False)
+    pulseStart = time.time()
+    pulseEnd = time.time()
+    counter = 0
+    while GPIO.input(ECHO) == 0:
+        pulseStart = time.time()
+        counter += 1
+    while GPIO.input(ECHO) == 1:
+        pulseEnd = time.time()
+
+    pulseDuration = pulseEnd - pulseStart
+    distance = pulseDuration * 17150
+    distance = round(distance, 2)
+    return distance
+
+
+# Configuration
+print("trigger: [{}]".format(trigger0))
+GPIO.setup(trigger0, GPIO.OUT) #Trigger
+print("echo: [{}]".format(echo0))
+GPIO.setup(echo0, GPIO.IN)  #Echo
+GPIO.output(trigger0, False)
+
+print("trigger: [{}]".format(trigger0))
+GPIO.setup(trigger1, GPIO.OUT) #Trigger
+print("echo: [{}]".format(echo1))
+GPIO.setup(echo1, GPIO.IN)  #Echo
+GPIO.output(trigger1, False)
+
+print("Setup completed!")
+
+distance0 = distance_measurement(trigger0, echo0)
+distance1 = distance_measurement(trigger1, echo1)
+
+while True:
+    print("Distance of U0: [{}] cm.".format(distance0))
+    time.sleep(0.5)
+    print("Distance of U1: [{}] cm.".format(distance1))
+    time.sleep(0.5)
+    if (distance0 <= 5) or (distance1 <= 5):
+        print("Emergency break state initiated.")
+        brake = True
+        print(brake)
+        break
+    else:
+        distance0 = distance_measurement(trigger0, echo0)
+        distance1 = distance_measurement(trigger1, echo1)
+
+GPIO.cleanup()
+print("Done")
